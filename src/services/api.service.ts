c0b017050a478d01931a28d515a24798770697c0
Sofiia Matveeva <sofia.matveeva@office.e2e4gu.ru>
2023-07-30 18:34:48 +0200

    fix: update coordinates types

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -7,13 +7,13 @@ export default class ApiService {
   private _apiKey = "c46753eb451a07012130f506fff5f978";
 
   public getWeatherData(
-    latitude: string,
-    longitude: string
+    latitude: number,
+    longitude: number
   ): Promise<AxiosResponse<WeatherApiResponse>> {
     const params = new URLSearchParams();
 
-    params.set("lat", latitude);
-    params.set("lon", longitude);
+    params.set("lat", latitude.toString());
+    params.set("lon", longitude.toString());
     params.set("appid", this._apiKey);
 
     const url = `${this._getWeatherUrl}?${params.toString()}`;
