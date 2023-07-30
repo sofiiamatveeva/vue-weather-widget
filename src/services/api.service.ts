af00885a1a5405ea756b650c71d75513821c02c9
Sofiia Matveeva <sofia.matveeva@office.e2e4gu.ru>
2023-07-30 19:04:27 +0200

    feat: add request for fetching location info

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,9 +1,11 @@
 import axios, { AxiosResponse } from "axios";
-import { BASE_WEATHER_URL } from "@/constants/api.constant";
+import { BASE_GEO_URL, BASE_WEATHER_URL } from "@/constants/api.constant";
 import { WeatherApiResponse } from "@/interfaces/weather.interfaces";
+import { LocationInfo } from "@/interfaces/location.interfaces";
 
 export default class ApiService {
   private _getWeatherUrl: string = BASE_WEATHER_URL;
+  private _getGeoInfoUrl: string = BASE_GEO_URL;
   private _apiKey = "c46753eb451a07012130f506fff5f978";
 
   public getWeatherData(
@@ -17,5 +19,16 @@ export default class ApiService {
     const url = `${this._getWeatherUrl}?${params.toString()}`;
 
     return axios.get<WeatherApiResponse>(url);
+  }
+
+  public getGeoInfo(cityName: string): Promise<AxiosResponse<LocationInfo[]>> {
+    const params = new URLSearchParams();
+
+    params.set("q", cityName);
+    params.set("appid", this._apiKey);
+
+    const url = `${this._getGeoInfoUrl}?${params.toString()}`;
+
+    return axios.get<LocationInfo[]>(url);
   }
 }
